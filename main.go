b2513f39ab6b72b3223286dec31a50dc7358b3d4
Jose Emmanuel Encarnacion Santana <jose.encarnacion@hit.com.do>
2022-04-04 10:20:03 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func helloWorld(c *fiber.Ctx) error {
+	return c.SendString("Hello new World")
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", helloWorld)
+
+	app.Listen(":3001")
+}
